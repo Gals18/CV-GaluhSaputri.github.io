a6cde915bff468436689c25a63c03578c081a28e
Gals18 <galuhsaputrii18@gmail.com>
2024-02-23 21:48:34 +0700

    first-commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// // Function to add fadeIn animation effect
+// function fadeIn(element) {
+//   element.style.opacity = 0;
+
+//   let opacity = 0;
+//   const interval = setInterval(() => {
+//     if (opacity < 1) {
+//       opacity += 0.1;
+//       element.style.opacity = opacity;
+//     } else {
+//       clearInterval(interval);
+//     }
+//   }, 100);
+// }
+
+// // Apply fadeIn effect to sidebar and main-content
+// const sidebar = document.getElementById("sidebar");
+// const mainContent = document.getElementById("main-content");
+
+// fadeIn(sidebar);
+// fadeIn(mainContent);
