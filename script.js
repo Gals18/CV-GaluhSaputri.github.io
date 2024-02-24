f10359837bd64b741ef100807ce0cfe54aa2116f
Gals18 <galuhsaputrii18@gmail.com>
2024-02-24 13:37:54 +0700

    second-commit

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-// // Function to add fadeIn animation effect
-// function fadeIn(element) {
-//   element.style.opacity = 0;
+// Function to add fadeIn animation effect
+const sidebar = document.getElementById("sidebar");
+const mainContent = document.getElementById("content");
 
-//   let opacity = 0;
-//   const interval = setInterval(() => {
-//     if (opacity < 1) {
-//       opacity += 0.1;
-//       element.style.opacity = opacity;
-//     } else {
-//       clearInterval(interval);
-//     }
-//   }, 100);
-// }
+fadeIn(sidebar);
+fadeIn(mainContent);
+function fadeIn(element) {
+  element.style.opacity = 0;
 
-// // Apply fadeIn effect to sidebar and main-content
-// const sidebar = document.getElementById("sidebar");
-// const mainContent = document.getElementById("main-content");
+  let opacity = 0;
+  const interval = setInterval(() => {
+    if (opacity < 1) {
+      opacity += 0.1;
+      element.style.opacity = opacity;
+    } else {
+      clearInterval(interval);
+    }
+  }, 100);
+}
+fadeIn(sidebar);
+fadeIn(mainContent);
+// Apply fadeIn effect to sidebar and main-content
 
-// fadeIn(sidebar);
-// fadeIn(mainContent);
+document.addEventListener("DOMContentLoaded", function() {
+    // Pesan yang ingin disimpan
+    var pesan = "Halo Galuh Saputri, Saya Tertarik dengan CV anda, Bisakah Untuk meluangkan waktu mengobrol dengan saya.";
+
+    // Nomor WhatsApp dan pesan yang disimpan
+    var nomorWhatsApp = "6281227507876";
+    var linkWhatsApp = "https://wa.me/" + nomorWhatsApp + "/?text=" + encodeURIComponent(pesan);
+
+    // Mengubah href dari tautan WhatsApp
+    document.getElementById("whatsappLink").setAttribute("href", linkWhatsApp);
+});
